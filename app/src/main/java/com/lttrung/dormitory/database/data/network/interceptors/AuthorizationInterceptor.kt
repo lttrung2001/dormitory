746d3790e907677e48b8613aa8964a5b3d7aac45
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-04-03 20:31:53 +0700

    handle response interceptor

--- app/src/main/java/com/lttrung/dormitory/database/data/network/interceptors/AuthorizationInterceptor.kt
+++ app/src/main/java/com/lttrung/dormitory/database/data/network/interceptors/AuthorizationInterceptor.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import com.lttrung.dormitory.ui.login.LoginActivity
 import com.lttrung.dormitory.utils.AppConstants
+import com.lttrung.dormitory.utils.HttpStatusCodes
 import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -20,14 +21,20 @@ class AuthorizationInterceptor @Inject constructor(
     override fun intercept(chain: Interceptor.Chain): Response {
         val newBuilder = chain.request().newBuilder()
         val accessToken = sharedPreferences.getString(AppConstants.ACCESS_TOKEN, "")
-        return if (accessToken!!.isEmpty()) {
+        return if (accessToken.isNullOrEmpty()) {
             val loginIntent = Intent(applicationContext, LoginActivity::class.java)
             loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             applicationContext.startActivity(loginIntent)
             throw IOException("Invalid token")
         } else {
             val newRequest: Request = newBuilder.addHeader("Authorization", accessToken).build()
-            chain.proceed(newRequest)
+            val response = chain.proceed(newRequest)
+            if (response.code == HttpStatusCodes.UNAUTHORIZED) {
+                applicationContext.startActivity(Intent(applicationContext, LoginActivity::class.java))
+                throw IOException("Expired or invalid token.")
+            } else {
+                return response
+            }
         }
     }
 }
